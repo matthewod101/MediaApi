a8f8aa9d140b7e5ec4ca6627868d8ee78a3c7553
matthewod101 <38329959+matthewod101@users.noreply.github.com>
2020-07-03 11:37:58 -0400

    Error Messages on Friday

--- MediaApi/Controllers/MediaController.cs
+++ MediaApi/Controllers/MediaController.cs
@@ -18,6 +18,73 @@ namespace MediaApi.Controllers
             Context = context;
         }
 
+        [HttpPost("media")]
+        public async Task<IActionResult> AddMedia([FromBody] PostMediaRequest mediaToAdd)
+        {
+
+            // ADD A FAKE DELAY to simulate a more "real world"
+            await Task.Delay(3000);
+            // 1. Make sure it is valid.
+            //  -- If it is not, return a 400 with some error information if you want.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                // 2. Add it to the database.
+                var media = new MediaItem
+                {
+                    Title = mediaToAdd.Title,
+                    Kind = mediaToAdd.Kind,
+                    Consumed = false,
+                    DateConsumed = null,
+                    RecommendedBy = mediaToAdd.RecommendedBy,
+                    Removed = false
+                };
+                Context.MediaItems.Add(media);
+                await Context.SaveChangesAsync();
+                var response = new MediaResponseItem
+                {
+                    Id = media.Id,
+                    Title = media.Title,
+                    Consumed = media.Consumed,
+                    DateConsumed = media.DateConsumed,
+                    Kind = media.Kind,
+                    RecommendedBy = media.RecommendedBy
+                };
+                // 3. Return a 201 (Created) status code.
+                // 4. Return a Location header with the URL of the new resource
+                // 5. It's nice just to give them a copy of the new resource.
+                // TEMP.
+                return CreatedAtRoute("media#getbyid", new { id = response.Id }, response);
+            }
+            
+        }
+
+        [HttpGet("media/{id:int}", Name = "media#getbyid")]
+        public async Task<IActionResult> GetAMediaItem(int id)
+        {
+            var item = await Context.MediaItems
+                .Where(m => m.Removed == false && m.Id == id)
+                .Select(m => new MediaResponseItem
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    RecommendedBy = m.RecommendedBy,
+                    Consumed = m.Consumed,
+                    DateConsumed = m.DateConsumed,
+                    Kind = m.Kind
+                }).SingleOrDefaultAsync();
+            if(item == null)
+            {
+                return NotFound("No item with that id.");
+            } else
+            {
+                return Ok(item);
+            }
+        }
+
 
 
         // GET /media?kind=game
---
+++ MediaApi/Models/PostMediaRequest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaApi.Models
+{
+    public class PostMediaRequest : IValidatableObject
+    {
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; }
+        [Required]
+        public string RecommendedBy { get; set; }
+        [Required]
+        public string Kind { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Title.ToLower() == "cats!!")
+            {
+                yield return new ValidationResult("That show stinks!", new string[] { "Kind" });
+            }
+            if(Kind != "show" && Kind != "game")
+            {
+                yield return new ValidationResult(
+                    "Must be a show or a game", new string[] { "Kind" });
+            }
+        }
+    }
+}
+
+
